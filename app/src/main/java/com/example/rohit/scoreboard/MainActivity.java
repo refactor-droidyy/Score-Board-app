864ebaaefa62cd46e3b1bebafd597bb44a8c59a8
rohitverma1999hack <rohitvermaavcbbk2011@gmail.com>
2019-11-03 15:52:42 +0530

    Get Set Foss

--- app/src/main/java/com/example/rohit/scoreboard/MainActivity.java
+++ app/src/main/java/com/example/rohit/scoreboard/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.rohit.scoreboard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button three,two,one,three_b,two_b,one_c,reset;
     int i=0;
     int j=0;
 
@@ -16,51 +19,103 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //displayForTeamA(0);
+        three = findViewById(R.id.three);
+        two = findViewById(R.id.two);
+        one = findViewById(R.id.one);
+        three_b = findViewById(R.id.threeb);
+        two_b = findViewById(R.id.twob);
+        one_c = findViewById(R.id.oneb);
+        reset = findViewById(R.id.reset);
+
+
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incrementThree();
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incrementTwo();
+            }
+        });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incrementOne();
+            }
+        });
+
+        three_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incrementThreeb();
+            }
+        });
+
+        two_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incrementTwob();
+            }
+        });
+
+        one_c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incrementOneb();
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // create methid for reset
+            }
+        });
+
     }
 
-    public void incrementThree(View view)
+    private void incrementThree()
     {
         i+=3;
         displayForTeamA(i);
     }
 
-    public void incrementTwo(View view)
+    private void incrementTwo()
     {
         i+=2;
         displayForTeamA(i);
     }
 
-    public void incrementOne(View view){
+    private void incrementOne(){
         i+=1;
         displayForTeamA(i);
     }
-    public void setzero(View view){
-        i = 0;
-        j=0;
-        displayForTeamA(i);
-        displayForTeamB(j);
-    }
 
-    public void incrementThreeb(View view){
+
+    private void incrementThreeb(){
         j+=3;
-        displayForTeamB(j);
+        displayForTeamA(j);
     }
 
-    public void incrementTwob(View view){
+    private void incrementTwob(){
         j+=2;
         displayForTeamB(j);
     }
-    public void incrementOneb(View view){
+    private void incrementOneb(){
         j+=1;
         displayForTeamB(j);
     }
-    public void displayForTeamA(int score) {
+    private void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    public void displayForTeamB(int score){
+    private void displayForTeamB(int score){
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
